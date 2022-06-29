61db14301b8fe1cc9e5f7d7d4cae4548b78875cc
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-30 01:05:40 +0530

    number patterns

--- src/patterns/Patterns.java
+++ src/patterns/Patterns.java
@@ -1,7 +1,68 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Patterns {
     public static void main(String[] args) {
+        int i = 1;
+        int j = 1;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
+        /**
+         *        1
+         *      3 2
+         *    6 5 4
+         * 10 9 8 7
+         */
+
+        while (i <= n) {
+            int space = 1;
+            while (space <= n - i){
+                System.out.print(" ");
+                space++;
+            }
+            int k = 1;
+            while(k <= i){
+                System.out.print(j);
+                j++;
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+
+        /**
+         * 1
+         * 2 3
+         * 4 5 6
+         * 7 8 9 10
+         while(i <= n){
+         int k = 1;
+         while(k <= i){
+         System.out.print(j + " ");
+         j++;
+         k++;
+         }
+         System.out.println();
+         i++;
+         }
+         */
+
+        /**
+         * 1
+         * 1 2
+         * 1 2 3
+         * 1 2 3 4
+         while (i <= n){
+         int k = 1;
+         while (k <= i){
+         System.out.print(k + " ");
+         k++;
+         }
+         System.out.println();
+         i++;
+         }
+         */
     }
 }
