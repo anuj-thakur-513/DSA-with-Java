517ae2478ce53fedc59929a73b08ccf0c4410071
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-07-17 23:10:41 +0530

    print subsequences optimal

--- src/recursion/Subsequences.java
+++ src/recursion/Subsequences.java
@@ -7,12 +7,13 @@ public class Subsequences {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
-
+        printSubsequences(input);
+        System.out.println();
         System.out.println(Arrays.toString(findSubsequences(input)));
     }
 
-    static String[] findSubsequences(String str){
-        if (str.length() == 0){
+    static String[] findSubsequences(String str) {
+        if (str.length() == 0) {
             return new String[]{""};
         }
 
@@ -29,5 +30,21 @@ public class Subsequences {
         }
 
         return ans;
+    }
+
+    // optimal solution without using extra space
+    static void printSubsequences(String str) {
+        printSubsequences(str, "");
+    }
+
+    static void printSubsequences(String str, String outputSoFar){
+        if (str.length() == 0){
+            System.out.println(outputSoFar);
+            return;
+        }
+        // we choose not to include the first character
+        printSubsequences(str.substring(1), outputSoFar);
+        // we choose to include the first character
+        printSubsequences(str.substring(1), outputSoFar + str.charAt(0));
     }
 }
