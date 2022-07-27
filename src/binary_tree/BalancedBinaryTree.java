95a34025f018299013d6d05e7d74a6c1b2a61728
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-07-28 04:02:26 +0530

    check balanced binary tree in efficient way

--- src/binary_tree/BalancedBinaryTree.java
+++ src/binary_tree/BalancedBinaryTree.java
@@ -28,4 +28,34 @@ public class BalancedBinaryTree {
 
         return isLeftBalanced && isRightBalanced;
     }
+
+    public static BalancedTreeReturn isBalancedBetter(BinaryTreeNode<Integer> root){
+        if (root == null){
+            int height = 0;
+            boolean isBal = true;
+            BalancedTreeReturn ans = new BalancedTreeReturn();
+            ans.height = height;
+            ans.isBalanced = isBal;
+            return ans;
+        }
+
+        BalancedTreeReturn leftOutput = isBalancedBetter(root.left);
+        BalancedTreeReturn rightOutput = isBalancedBetter(root.right);
+
+        boolean isBal = true;
+        int height = 1 + Math.max(leftOutput.height, rightOutput.height);
+
+        if (Math.abs(leftOutput.height - rightOutput.height) > 1){
+            isBal = false;
+        }
+
+        if (!leftOutput.isBalanced || !rightOutput.isBalanced){
+            isBal = false;
+        }
+
+        BalancedTreeReturn ans = new BalancedTreeReturn();
+        ans.height = height;
+        ans.isBalanced = isBal;
+        return ans;
+    }
 }
